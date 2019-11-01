09532ac109a46463050b56c0ce3cca6eaefec626
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-11-01 08:28:16 -0700

    improve test err logging (currentLib)

--- currentLib/test.js
+++ currentLib/test.js
@@ -4,4 +4,4 @@ const assert = require("assert");
 assert(constructorMethods);
 assert(bootstrap);
 assert(properties);
-console.log("Woo! Assertions passed!");
+console.log(`\u001b[32mWoo! Assertions passed! Successfully required from @truffle/contract/lib/contract/index.js\u001b[0m`);
